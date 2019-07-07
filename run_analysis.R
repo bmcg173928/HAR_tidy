ef9c79520e0be9418d0fe66a7a3cfb5eabe9aaa7
bmcg173928 <50603714+bmcg173928@users.noreply.github.com>
2019-07-07 10:34:47 -0400

    Add files via upload

---
+++ run_analysis.R
@@ -0,0 +1,69 @@
+# Merges the training and the test sets to create one data set.
+# Extracts only the measurements on the mean and standard deviation
+#   for each measurement.
+# Uses descriptive activity names to name the activities in the data set
+# Appropriately labels the data set with descriptive variable names.
+# From the data set in step 4, creates a second, independent tidy data set
+#   with the average of each variable for each activity and each subject.
+# This script assumes the Human Human Activity Recognition (HAR) Using Smartphones
+#   Dataset was downloaded, files extracted, and the working directory is the
+#   HAR parent directory.  Uncomment the first section if files were not
+#   previously downloaded.
+
+## download dataset - uncomment if dataset is not already downloaded
+# url<-"https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+# download.file(url, destfile="wearables.zip")
+# unzip("wearables.zip")
+# setwd("UCI HAR Dataset")
+
+## determine duplicate columns to be removed
+library(dplyr)
+features<-read.table("features.txt", header = FALSE, sep = "")
+features<-features[,2]
+duplicates<-duplicated(features)
+activity_labels <- read.table("activity_labels.txt", header = FALSE, sep = "")
+
+## load test dataset
+setwd("test")
+xtest<-read.table("X_test.txt", header = FALSE, sep = "")
+
+## add descriptive labels to test dataset
+names(xtest) <- features
+ytest<-read.csv("y_test.txt", header = FALSE, sep = "")
+names(ytest) <- "activity"
+idtest<-read.csv("subject_test.txt", header = FALSE, sep = "")
+names(idtest) <- "subject"
+test<-cbind(idtest, ytest, xtest)
+
+## load train dataset
+setwd("..")
+setwd("train")
+xtrain<-read.csv("X_train.txt", header = FALSE, sep = "")
+
+## add descriptive labels to train dataset
+names(xtrain) <- features
+ytrain<-read.csv("y_train.txt", header = FALSE, sep = "")
+idtrain<-read.csv("subject_train.txt", header = FALSE, sep = "")
+names(ytrain) <- "activity"
+names(idtrain) <- "subject"
+train <- cbind(idtrain, ytrain, xtrain)
+
+## part 1 - combine datasets
+HAR_tidy_temp <- rbind(train, test)
+
+## part 2 - extract means and standard deviations
+HAR_tidy_temp<-HAR_tidy_temp[,!duplicates]
+HAR_tidy_temp <- select(HAR_tidy_temp,matches("activity|subject|mean\\()|std\\()", ignore.case = TRUE))
+
+## part 3 - add descriptive activity names to dataset
+HAR_tidy_temp$activity <- activity_labels[match(HAR_tidy_temp$activity, activity_labels[,1]),2]
+
+## part 5 - create tidy dataset and save
+HAR_tidy <- HAR_tidy_temp %>% group_by(subject, activity) %>% summarize_all(list(mean))
+HAR_tidy
+setwd("..")
+write.table(HAR_tidy, row.names = FALSE, "HAR_tidy.txt")
+
+## remove unused variables
+rm(activity_labels, idtest, idtrain, test, train, xtest, xtest2, xtest3, xtrain, ytest, ytrain)
+rm(duplicates, features, features1, mywd, url, HAR_tidy_temp)
